648b5056b61dd361bbaaf6b97d801a331eee72a4
Yufeng Ying <nbyyf2002@mail.ustc.edu.cn>
2022-04-12 10:57:21 +0800

    Initial commit

---
+++ mirror_site.py
@@ -0,0 +1,104 @@
+#!/bin/python3
+
+import os
+from os import path, cpu_count
+import subprocess
+import argparse
+from functools import reduce
+from itertools import chain
+from multiprocessing import Pool
+from utils import convert_link, convert_image
+
+
+def convert_links(n_thread: int):
+    workload = reduce(
+        chain,
+        map(
+            lambda walk: map(lambda file: path.join(walk[0], file), walk[2]),
+            os.walk("./icourse.club/course"),
+        ),
+    )
+
+    with Pool(n_thread) as p:
+        p.map(convert_link, workload)
+
+
+def convert_images(n_thread: int):
+    workload = filter(
+        lambda file: path.splitext(file[1])[1] != ".webp",
+        reduce(
+            chain,
+            map(
+                lambda walk: map(lambda file: path.join(walk[0], file), walk[2]),
+                os.walk("./icourse.club/uploads/images"),
+            ),
+        ),
+    )
+
+    with Pool(n_thread) as p:
+        p.map(convert_image, workload)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--min", help="min popular page to fetch", action="store")
+    parser.add_argument(
+        "--max", help="max popular page to fetch", action="store", required=True
+    )
+    parser.add_argument("-n", help="threads to use in download", action="store")
+    parser.add_argument(
+        "--convert-links",
+        help="convert links in html file",
+        action="store_const",
+        const=True,
+    )
+    parser.add_argument(
+        "--convert-images",
+        help="convert images downloaded to webp",
+        action="store_const",
+        const=True,
+    )
+    args = parser.parse_args()
+
+    if not os.path.exists("./icourse.club/uploads/images"):
+        os.makedirs("./icourse.club/uploads/images")
+    if not os.path.exists("./icourse.club/static/image"):
+        os.makedirs("./icourse.club/static/image")
+    if not os.path.exists("./icourse.club/course"):
+        os.makedirs("./icourse.club/course")
+
+    env = os.environ.copy()
+    env["RUST_LOG"] = "info"
+    if os.path.exists("./mirrorsite"):
+        subprocess.run(
+            [
+                "./mirrorsite",
+                "--min={}".format(args.min if args.min is not None else 1),
+                "--max={}".format(args.max),
+                "-n={}".format(args.n if args.n is not None else 5),
+            ],
+            check=True,
+            env=env,
+        )
+    elif os.path.exists("./target/release/mirrorsite"):
+        subprocess.run(
+            [
+                "cargo",
+                "run",
+                "--release",
+                "--",
+                "--min={}".format(args.min if args.min is not None else 1),
+                "--max={}".format(args.max),
+                "-n={}".format(args.n if args.n is not None else 5),
+            ],
+            check=True,
+            env=env,
+        )
+    else:
+        raise Exception("Cannot find mirrorsite binary")
+
+    if args.convert_links:
+        convert_links(cpu_count())
+
+    if args.convert_images:
+        convert_images(cpu_count())
---
+++ utils.py
@@ -0,0 +1,53 @@
+#!/bin/python3
+import os
+from PIL import Image
+from bs4 import BeautifulSoup
+import minify_html
+
+
+def convert_image(path: str):
+    name, ext = os.path.splitext(path)
+    assert ext != ".webp"
+    print("Convert image", os.path.split(path)[1])
+    try:
+        image = Image.open(path)
+        image.convert("RGB")
+        image.save(
+            name + ".webp",
+            format="webp",
+            optimize=True,
+            quality=50,
+            method=6,
+        )
+        os.remove(path)
+    except Exception as e:
+        print("error occured in image process: {}".format(e))
+
+
+def convert_link(path: str):
+    print(f"Convert links in {path}")
+    with open(path, "r", encoding="utf-8") as file:
+        soup = BeautifulSoup(file, features="lxml")
+        # special treat style.css, because '?' seems to be reserved in browser environment
+        style = soup.select('link[href^="/static/css/style.css"]')
+        if len(style) >= 1:
+            assert len(style) == 1
+            style = style[0]
+            style["href"] = style["href"].split("?")[0]
+        # redirect static resources to local files
+        for static in soup.select('[href^="/static"]'):
+            static["href"] = "../.." + static["href"]
+        # tag_a are mostly links to courses, teachers, users
+        # since we do not mirror these things, simply redirect them to origin site
+        for tag in soup.select('a[href^="/"]'):
+            tag["href"] = "https://icourse.club" + tag["href"]
+        # redirect image src to local files
+        for img in soup.select('img[href^="/uploads/images"]'):
+            img["src"] = "../.." + img["src"].split(".")[0] + ".webp"
+        # remove javascripts to reduce file size
+        for script in soup.find_all("script"):
+            script.decompose()
+
+    # compress text to reduce size
+    with open(path, "w") as file:
+        file.write(minify_html.minify(str(soup), remove_processing_instructions=True))
